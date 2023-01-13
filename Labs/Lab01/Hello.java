5aecd8742e0fab1a3b7069afefaaf568ea7ea650
Senay Tilahun <senay.argaw@gmail.com>
2023-01-13 10:56:33 -0800

    Hello World update

--- Labs/Lab01/Hello.java
+++ Labs/Lab01/Hello.java
@@ -1,9 +1,8 @@
-/** First Java Program
- *
- * */
-
+/** First Java Program */
+package Labs.Lab01;
 public class Hello {
+    // Prints "Hello, World" to the terminal window.
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello, World!");
     }
 }
