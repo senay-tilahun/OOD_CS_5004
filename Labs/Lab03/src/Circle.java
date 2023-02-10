c0782987c2aa094e53f7a21dd9ea58d5787aaaed
Senay Tilahun <senay.argaw@gmail.com>
2023-02-10 15:05:37 -0800

    TA comment updates

--- Labs/Lab03/src/Circle.java
+++ Labs/Lab03/src/Circle.java
@@ -35,33 +35,66 @@ public class Circle implements Shape {
     ..this.centery : double
     ..this.radius  : double
   */
+
+  /**
+   * Creates a string representation of this string
+   * @return a string representation of this string
+   */
   public String toString() {
     return String.format("Circle: center (%.3f,%.3f) radius %.3f",
         this.centerx,this.centery,this.radius);
   }
 
+  /**
+   * Returns the distance of this shape from the origin. The distance is
+   * measured from whatever reference position a shape is (e.g. a center for
+   * a circle)
+   * @return the distance from the origin
+   */
   @Override
   public double distanceFromOrigin() {
     return Math.sqrt(this.centerx * this.centerx +
                      this.centery * this.centery);
   } // Repetitive.
 
+  /**
+   * Computes and returns the area of this shape.
+   * @return the area of the shape
+   */
   @Override
   public double area() {
     return Math.PI * radius * radius;
   }
 
+  /**
+   * Computes and returns the perimeter of this shape.
+   * @return the perimeter of the shape
+   */
   @Override
   public double perimeter() {
     return 2 * Math.PI * radius;
   }
 
+  /**
+   * Create and return a shape of the same kind as this one, resized
+   * in area by the provided factor
+   * @param factor factor of resizing
+   * @return the resized Shape
+   */
   @Override
   public Shape resize(double factor) {
     return new Circle(this.centerx, this.centery, Math.sqrt(factor) *
         radius);
   }
 
+  /**
+   * Compares this shape with the one passed to it based on their areas.
+   * if (this<s) return a negative number
+   * if (this==s) return 0
+   * if (this>s) return a positive number
+   * @param s the other shape to be compared to
+   * @return the result of the comparison
+   */
   @Override
   public int compareTo(Shape s) {
     double areaThis = this.area();
--- Labs/Lab03/src/Rectangle.java
+++ Labs/Lab03/src/Rectangle.java
@@ -28,27 +28,52 @@ public class Rectangle implements Shape{
     ..this.width  : double
     ..this.height : double
   */
+
+  /**
+   * Creates a string representation of this string
+   * @return a string representation of this string
+   */
   public String toString() {
     return String.format("Rectangle: LL corner (%.3f,%.3f) width %.3f height " +
             "%.3f",
         this.x,this.y,this.width,this.height);
   }
 
+  /**
+   * Returns the distance of this shape from the origin. The distance is
+   * measured from whatever reference position a shape is (e.g. a center for
+   * a circle)
+   * @return the distance from the origin
+   */
   @Override
   public double distanceFromOrigin() {
     return Math.sqrt(this.x * this.x + this.y * this.y);
   } // Repetitive.
 
+  /**
+   * Computes and returns the area of this shape.
+   * @return the area of the shape
+   */
   @Override
   public double area() {
     return this.width * this.height;
   }
 
+  /**
+   * Computes and returns the perimeter of this shape.
+   * @return the perimeter of the shape
+   */
   @Override
   public double perimeter() {
     return 2 * (this.width + this.height);
   }
 
+  /**
+   * Create and return a shape of the same kind as this one, resized
+   * in area by the provided factor
+   * @param factor factor of resizing
+   * @return the resized Shape
+   */
   @Override
   public Shape resize(double factor) {
     double sqrtFactor = Math.sqrt(factor);
@@ -59,6 +84,14 @@ public class Rectangle implements Shape{
         sqrtFactor * this.height);
   }
 
+  /**
+   * Compares this shape with the one passed to it based on their areas.
+   * if (this<s) return a negative number
+   * if (this==s) return 0
+   * if (this>s) return a positive number
+   * @param s the other shape to be compared to
+   * @return the result of the comparison
+   */
   @Override
   public int compareTo(Shape s) {
     double areaThis = this.area();
