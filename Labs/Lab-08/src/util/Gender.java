36d72aea9666421041f2e0d18e310593681597fe
Senay Tilahun <senay.argaw@gmail.com>
2023-03-23 15:26:09 -0700

    lab08

---
+++ Labs/Lab-08/src/util/Gender.java
@@ -0,0 +1,10 @@
+package util;
+
+/**
+ * This enumeration type represents the gender of a single employee, as
+ * disclosed by the employee in the official employment record
+ */
+
+public enum Gender {
+  Male,Female,UnDisclosed
+}
---
+++ Labs/Lab08/Gender.java
@@ -0,0 +1,10 @@
+package organization;
+
+/**
+ * This enumeration type represents the gender of a single employee, as
+ * disclosed by the employee in the official employment record
+ */
+
+public enum Gender {
+  Male,Female,UnDisclosed
+}
