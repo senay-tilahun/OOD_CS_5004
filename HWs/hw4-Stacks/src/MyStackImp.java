9e95fb02830871699b309d49a05b2f112189879d
Senay Tilahun <senay.argaw@gmail.com>
2023-03-23 16:32:57 -0700

    updated java docs

--- HWs/hw4-Stacks/src/MyStackImp.java
+++ HWs/hw4-Stacks/src/MyStackImp.java
@@ -1,24 +1,43 @@
 
 import java.util.*;
 
+/**
+ * This class represents a generic implementation of MyStack.
+ * @param <T> the type of data containing in the stack during use
+ */
 public class MyStackImp<T> implements MyStack<T> {
   private List<T> myStack;
 
-
+  /**
+   * Constructor 1
+   * Constructs a MyStackImp with an initial size
+   * @param size the initial size of MyStack
+   */
   public MyStackImp(int size) {
     this.myStack = new ArrayList<>(8);
   }
 
+  /**
+   * Constructor 2
+   * Constructs an empty stack
+   */
   public MyStackImp(){
     this.myStack = new ArrayList<>();
   }
 
-
+  /**
+   * Adds element to the stack
+   * @param item the item to add
+   */
   @Override
   public void push(T item) {
     myStack.add(item);
   }
 
+  /**
+   * Removes the last element of the stack
+   * @return the last element of the stack
+   */
   @Override
   public T pop() {
     if (this.empty()){
@@ -27,6 +46,10 @@ public class MyStackImp<T> implements MyStack<T> {
     return myStack.remove(myStack.size() - 1);
   }
 
+  /**
+   * Peeks and returns the last element of the stack without removing it
+   * @return the last element of the stack
+   */
   @Override
   public T top() {
     if (this.empty()){
@@ -35,15 +58,27 @@ public class MyStackImp<T> implements MyStack<T> {
     return myStack.get(myStack.size() - 1);
   }
 
+  /**
+   * Checks if the stack is empty
+   * @return boolean, indicating whether the stack is empty of not
+   */
   @Override
   public boolean empty() {
     return myStack.isEmpty();
   }
 
+  /**
+   * Getter method for the instance variable
+   * @return the List myStack
+   */
   public List<T> getMyStack() {
     return myStack;
   }
 
+  /**
+   * Method to create the string representation of MyStack
+   * @return the string representation of MySta
+   */
   @Override
   public String toString() {
     StringBuilder builder = new StringBuilder();
--- HWs/hw4-Stacks/test/MyStackImpTest.java
+++ HWs/hw4-Stacks/test/MyStackImpTest.java
@@ -7,16 +7,25 @@ import org.junit.Test;
 public class MyStackImpTest {
 
   MyStackImp<Integer> intStack;
+  MyStackImp<Integer> intStack2;
 
   MyStackImp<String> strStack;
 
   MyStackImp<Double> dblStack;
 
+  /**
+   * Set up rest of unittest
+   * Instantiate four MyStackImp
+   * @throws Exception general
+   */
   @Before
   public void testSetUp() throws Exception {
     intStack = new MyStackImp<>();
     strStack = new MyStackImp<>();
     dblStack = new MyStackImp<>();
+
+    // test Constructor 1
+    intStack2 = new MyStackImp<>(3);
 
     intStack.push(1);
     intStack.push(2);
@@ -27,6 +36,9 @@ public class MyStackImpTest {
 
   }
 
+  /**
+   * Test to confirm the push method
+   */
   @Test
   public void testPush() {
     // also tests getMyStack method
@@ -35,6 +47,9 @@ public class MyStackImpTest {
     assertEquals(1, dblStack.getMyStack().size());
   }
 
+  /**
+   * Test to confirm the pop method
+   */
   @Test
   public void testPop() {
     // also tests getMyStack method
@@ -46,18 +61,27 @@ public class MyStackImpTest {
     assertEquals(2, intStack.getMyStack().size());
   }
 
+  /**
+   * Test to confirm the EmptyStackException of pop method
+   */
   @Test (expected = EmptyStackException.class)
   public void testPopException() {
     strStack.pop();
     strStack.pop();
   }
 
+  /**
+   * Test to confirm the EmptyStackException of yop method
+   */
   @Test (expected = EmptyStackException.class)
   public void testTopException() {
     dblStack.pop();
     dblStack.top();
   }
 
+  /**
+   * Test to confirm the top method
+   */
   @Test
   public void testTop() {
 
@@ -70,6 +94,9 @@ public class MyStackImpTest {
     assertEquals(1.0, actual3, 0.01);
   }
 
+  /**
+   * Test to confirm the empty method
+   */
   @Test
   public void testEmpty() {
     strStack.pop();
@@ -81,7 +108,9 @@ public class MyStackImpTest {
     assertFalse(intStack.empty());
   }
 
-
+  /**
+   * Test to confirm the toString method
+   */
   @Test
   public void testToString() {
     String one = "Stack: 1 2 3 4";
