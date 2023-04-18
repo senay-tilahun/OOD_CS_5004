f27d3b57236a591de8f1297a680ed61e1e8b005a
Senay Tilahun <senay.argaw@gmail.com>
2023-04-18 13:23:23 -0700

    initial commit

---
+++ Final Project/src/gitlet/Gitlet.java
@@ -0,0 +1,16 @@
+package gitlet;
+
+/**
+ * Class that acts as our local repository
+ * Sets our staging area and our commit tree
+ * Sets our head and main branches / pointers
+ * Includes methods to
+ *  add new commits objects & commit changes to our commit tree
+ *  find previous commits
+ *  find main & head branches
+ *  add and remove files from our staging area
+ *  clear our staging area
+ */
+public class Gitlet {
+
+}
---
+++ Final Project/src/gitlet/Main.java
@@ -0,0 +1,16 @@
+package gitlet;
+
+/**
+ * Main method, has one main method
+ * used to validate user args
+ * based on user args will perform different function of gitlet
+ * Acts as Controller in the MVC architecture
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    // TODO - check that the args are valid i.e run Valid.args from Valid class
+
+    // TODO - Switch statement for specific args
+  }
+}
